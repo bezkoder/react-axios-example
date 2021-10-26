4135f4ebac68026921b5f41d45284c75093b8a99
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-10-26 10:50:09 +0700

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,68 @@
-# Getting Started with Create React App
+# React Axios example with Rest API
+
+React Client with Axios to make CRUD requests to Rest API in that:
+
+- React Axios GET request: get all Tutorials, get Tutorial by Id, find Tutorial by title
+- React Axios POST request: create new Tutorial
+- React Axios PUT request: update an existing Tutorial
+- React Axios DELETE request: delete a Tutorial, delete all Tutorials
+
+![react-axios-example](react-axios-example.png)
+
+For instruction, please visit:
+> [React Axios example - Get/Post/Put/Delete with Rest API](https://www.bezkoder.com/react-axios-example/)
+
+Related Posts:
+> [React Fetch example - Get/Post/Put/Delete with Rest API](https://www.bezkoder.com/react-fetch-example/)
+
+> [React + Axios: CRUD example to consume Web API](https://www.bezkoder.com/react-crud-web-api/)
+
+> [React Table example: CRUD App with react-table v7](https://www.bezkoder.com/react-table-example-hooks-crud/)
+
+Using Material UI instead of Bootstrap:
+
+> [React Material UI examples with a CRUD Application](https://www.bezkoder.com/react-material-ui-examples-crud/)
+
+More Practice:
+
+> [React Pagination example](https://www.bezkoder.com/react-pagination-material-ui/)
+
+> [React File Upload example](https://www.bezkoder.com/react-file-upload-axios/)
+
+> [React JWT Authentication & Authorization example](https://www.bezkoder.com/react-jwt-auth/)
+
+> [React + Redux: JWT Authentication & Authorization example](https://www.bezkoder.com/react-redux-jwt-auth/)
+
+Fullstack with Node Express:
+
+> [React + Node Express + MySQL](https://www.bezkoder.com/react-node-express-mysql/)
+
+> [React + Node Express + PostgreSQL](https://www.bezkoder.com/react-node-express-postgresql/)
+
+> [React + Node Express + MongoDB](https://www.bezkoder.com/react-node-express-mongodb-mern-stack/)
+
+Fullstack with Spring Boot:
+
+> [React + Spring Boot + MySQL](https://www.bezkoder.com/react-spring-boot-crud/)
+
+> [React + Spring Boot + PostgreSQL](https://www.bezkoder.com/spring-boot-react-postgresql/)
+
+> [React + Spring Boot + MongoDB](https://www.bezkoder.com/react-spring-boot-mongodb/)
+
+Fullstack with Django:
+
+> [React + Django Rest Framework](https://www.bezkoder.com/django-react-axios-rest-framework/)
+
+Serverless:
+
+> [React Firebase CRUD App with Realtime Database](https://www.bezkoder.com/react-firebase-crud/)
+
+> [React Firestore CRUD App example | Firebase Cloud Firestore](https://www.bezkoder.com/react-firestore-crud/)
+
+Integration (run back-end & front-end on same server/port)
+
+> [How to integrate React with Spring Boot](https://www.bezkoder.com/integrate-reactjs-spring-boot/)
+
+> [Integrate React with Node Express on same Server/Port](https://www.bezkoder.com/integrate-react-express-same-server-port/)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,284 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+import apiClient from "./http-common";
 
 function App() {
+  const get_id = useRef(null);
+  const get_title = useRef(null);
+
+  const post_title = useRef(null);
+  const post_description = useRef(null);
+
+  const put_id = useRef(null);
+  const put_title = useRef(null);
+  const put_description = useRef(null);
+  const put_published = useRef(null);
+
+  const delete_id = useRef(null);
+
+  const [getResult, setGetResult] = useState(null);
+  const [postResult, setPostResult] = useState(null);
+  const [putResult, setPutResult] = useState(null);
+  const [deleteResult, setDeleteResult] = useState(null);
+
+  const fortmatResponse = (res) => {
+    return JSON.stringify(res, null, 2);
+  };
+
+  async function getAllData() {
+    try {
+      const res = await apiClient.get("/tutorials");
+
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: res.headers,
+        data: res.data,
+      };
+
+      setGetResult(fortmatResponse(result));
+    } catch (err) {
+      setGetResult(fortmatResponse(err.response?.data || err));
+    }
+  }
+
+  async function getDataById() {
+    const id = get_id.current.value;
+
+    if (id) {
+      try {
+        const res = await apiClient.get(`/tutorials/${id}`);
+
+        const result = {
+          data: res.data,
+          status: res.status,
+          statusText: res.statusText,
+          headers: res.headers,
+        };
+
+        setGetResult(fortmatResponse(result));
+      } catch (err) {
+        setGetResult(fortmatResponse(err.response?.data || err));
+      }
+    }
+  }
+
+  async function getDataByTitle() {
+    const title = get_title.current.value;
+
+    if (title) {
+      try {
+        // const res = await instance.get(`/tutorials?title=${title}`);
+        const res = await apiClient.get("/tutorials", {
+          params: {
+            title: title,
+          },
+        });
+
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: res.headers,
+          data: res.data,
+        };
+
+        setGetResult(fortmatResponse(result));
+      } catch (err) {
+        setGetResult(fortmatResponse(err.response?.data || err));
+      }
+    }
+  }
+
+  async function postData() {
+    const postData = {
+      title: post_title.current.value,
+      description: post_description.current.value,
+    };
+
+    try {
+      const res = await apiClient.post("/tutorials", postData, {
+        headers: {
+          "x-access-token": "token-value",
+        },
+      });
+
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: res.headers,
+        data: res.data,
+      };
+
+      setPostResult(fortmatResponse(result));
+    } catch (err) {
+      setPostResult(fortmatResponse(err.response?.data || err));
+    }
+  }
+
+  async function putData() {
+    const id = put_id.current.value;
+
+    if (id) {
+      const putData = {
+        title: put_title.current.value,
+        description: put_description.current.value,
+        published: put_published.current.checked,
+      };
+
+      try {
+        const res = await apiClient.put(`/tutorials/${id}`, putData, {
+          headers: {
+            "x-access-token": "token-value",
+          },
+        });
+
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: res.headers,
+          data: res.data,
+        };
+
+        setPutResult(fortmatResponse(result));
+      } catch (err) {
+        setPutResult(fortmatResponse(err.response?.data || err));
+      }
+    }
+  }
+
+  async function deleteAllData() {
+    try {
+      const res = await apiClient.delete("/tutorials");
+
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: res.headers,
+        data: res.data,
+      };
+
+      setDeleteResult(fortmatResponse(result));
+    } catch (err) {
+      setDeleteResult(fortmatResponse(err.response?.data || err));
+    }
+  }
+
+  async function deleteDataById() {
+    const id = delete_id.current.value;
+
+    if (id) {
+      try {
+        const res = await apiClient.delete(`/tutorials/${id}`);
+
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: res.headers,
+          data: res.data,
+        };
+
+        setDeleteResult(fortmatResponse(result));
+      } catch (err) {
+        setDeleteResult(fortmatResponse(err.response?.data || err));
+      }
+    }
+  }
+
+  const clearGetOutput = () => {
+    setGetResult(null);
+  };
+
+  const clearPostOutput = () => {
+    setPostResult(null);
+  };
+
+  const clearPutOutput = () => {
+    setPutResult(null);
+  };
+
+  const clearDeleteOutput = () => {
+    setDeleteResult(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="container my-3">
+      <h3>React Axios example</h3>
+
+      <div className="card mt-3">
+        <div className="card-header">React Axios GET - BezKoder.com</div>
+        <div className="card-body">
+          <div className="input-group input-group-sm">
+            <button className="btn btn-sm btn-primary" onClick={getAllData}>Get All</button>
+
+            <input type="text" ref={get_id} className="form-control ml-2" placeholder="Id" />
+            <div className="input-group-append">
+              <button className="btn btn-sm btn-primary" onClick={getDataById}>Get by Id</button>
+            </div>
+
+            <input type="text" ref={get_title} className="form-control ml-2" placeholder="Title" />
+            <div className="input-group-append">
+              <button className="btn btn-sm btn-primary" onClick={getDataByTitle}>Find By Title</button>
+            </div>
+
+            <button className="btn btn-sm btn-warning ml-2" onClick={clearGetOutput}>Clear</button>
+          </div>   
+          
+          { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }
+        </div>
+      </div>
+
+      <div className="card mt-3">
+        <div className="card-header">React Axios POST - BezKoder.com</div>
+        <div className="card-body">
+          <div className="form-group">
+            <input type="text" className="form-control" ref={post_title} placeholder="Title" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" ref={post_description} placeholder="Description" />
+          </div>
+          <button className="btn btn-sm btn-primary" onClick={postData}>Post Data</button>
+          <button className="btn btn-sm btn-warning ml-2" onClick={clearPostOutput}>Clear</button>
+
+          { postResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{postResult}</pre></div> }
+        </div>
+      </div>
+
+      <div className="card mt-3">
+        <div className="card-header">React Axios PUT - BezKoder.com</div>
+        <div className="card-body">
+          <div className="form-group">
+            <input type="text" className="form-control" ref={put_id} placeholder="Id" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" ref={put_title} placeholder="Title" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" ref={put_description} placeholder="Description" />
+          </div>
+          <div className="form-check mb-2">
+            <input type="checkbox" className="form-check-input" ref={put_published} />
+            <label className="form-check-label" htmlFor="put_published">Publish</label>
+          </div>
+          <button className="btn btn-sm btn-primary" onClick={putData}>Update Data</button>
+          <button className="btn btn-sm btn-warning ml-2" onClick={clearPutOutput}>Clear</button>
+
+          { putResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{putResult}</pre></div> }
+        </div>
+      </div>
+
+      <div className="card mt-3">
+        <div className="card-header">React Axios DELETE - BezKoder.com</div>
+        <div className="card-body">
+          <div className="input-group input-group-sm">
+            <button className="btn btn-sm btn-danger" onClick={deleteAllData}>Delete All</button>
+
+            <input type="text" ref={delete_id} className="form-control ml-2" placeholder="Id" />
+            <div className="input-group-append">
+              <button className="btn btn-sm btn-danger" onClick={deleteDataById}>Delete by Id</button>
+            </div>
+
+            <button className="btn btn-sm btn-warning ml-2" onClick={clearDeleteOutput}>Clear</button>
+          </div>    
+          
+          { deleteResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{deleteResult}</pre></div> }      
+        </div>
+      </div>
+ 
     </div>
   );
 }
